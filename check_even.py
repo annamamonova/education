e5f1c34646af065bc7f0dcff9e0d34adf564e89f
SEAnMa <Anna.Mamonova@ascom.com>
2019-03-13 18:26:23 +0300

    rewrite functions

--- check_even.py
+++ check_even.py
@@ -1,9 +1,3 @@
 # Test check number is even or odd
-def check_even(number):
-    if type(number) is int:
-        if number == 0:
-            return 'This is null!'
-        if number % 2 is 0:
-            return True
-        return False
-    return "Number isn't integer!"
+def is_even(number):
+    return number & 1 == 0
---
+++ unit_test.py
@@ -0,0 +1,15 @@
+import unittest
+from check_even import is_even
+
+
+class TestCheckEven(unittest.TestCase):
+
+    def test_even(self):
+        self.assertTrue(is_even(4))
+
+    def test_odd(self):
+        self.assertFalse(is_even(3))
+
+
+if __name__ == '__main__':
+    unittest.main()
