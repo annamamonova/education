62c8d5c168713bec9a5125d60fb284cfd800a71f
SEAnMa <Anna.Mamonova@ascom.com>
2019-03-15 14:11:51 +0300

    rewrite "print"

--- task_print_ab_sequence/print_ab_sequence.py
+++ task_print_ab_sequence/print_ab_sequence.py
@@ -1,12 +1,15 @@
 # print 1 2a 3b 4a 5 6ab 7 8a 9b 10a 11 12ab ... 100a sequence
 def print_ab_sequence():
+    sequence = ''
     for number in range(1, 101):
         letters = ""
         if number % 2 == 0:
             letters = "a"
         if number % 3 == 0:
             letters += "b"
-        print(number, letters, sep='', end=' ')
+        sequence += str(number) + letters + ' '
+    print(sequence)
 
 
-print_ab_sequence()
+if __name__ == '__main__':
+    print_ab_sequence()
