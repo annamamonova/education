e6834c8e99c1e485546db8c373bd5f52ea287cc1
SEAnMa <Anna.Mamonova@ascom.com>
2019-03-18 13:56:33 +0300

    little change

--- task_check_even/unit_test.py
+++ task_check_even/unit_test.py
@@ -1,12 +1,16 @@
+# unittests for check even
 import unittest
 from task_check_even.check_even import is_even
 
 
+# unittests for check even
 class TestCheckEven(unittest.TestCase):
 
+    # test for check even
     def test_even(self):
         self.assertTrue(is_even(4))
 
+    # test for check odd
     def test_odd(self):
         self.assertFalse(is_even(3))
 
