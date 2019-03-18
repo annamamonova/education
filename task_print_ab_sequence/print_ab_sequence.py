1981ffeb2be0c54f61f3f103936ffe4ce4bed595
SEAnMa <Anna.Mamonova@ascom.com>
2019-03-18 11:14:19 +0300

    write unittest

--- task_print_ab_sequence/print_ab_sequence.py
+++ task_print_ab_sequence/print_ab_sequence.py
@@ -1,15 +1,15 @@
 # print 1 2a 3b 4a 5 6ab 7 8a 9b 10a 11 12ab ... 100a sequence
-def print_ab_sequence():
+def print_ab_sequence(number_min, number_max):
     sequence = ''
-    for number in range(1, 101):
+    for number in range(number_min, number_max + 1):
         letters = ""
         if number % 2 == 0:
             letters = "a"
         if number % 3 == 0:
             letters += "b"
         sequence += str(number) + letters + ' '
-    print(sequence)
+    return sequence
 
 
 if __name__ == '__main__':
-    print_ab_sequence()
+    print(print_ab_sequence())
