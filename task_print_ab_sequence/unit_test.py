769c619fbc27cca5675bf310df8be68025df6643
SEAnMa <Anna.Mamonova@ascom.com>
2019-03-18 13:04:18 +0300

    little change

--- task_print_ab_sequence/unit_test.py
+++ task_print_ab_sequence/unit_test.py
@@ -2,7 +2,7 @@ import unittest
 from task_print_ab_sequence.print_ab_sequence import print_ab_sequence
 
 
-class TestPrintABSequence(unittest.TestCase):
+class TestPrintAbSequence(unittest.TestCase):
 
     def test_sequence(self):
         self.assertEqual(print_ab_sequence(1, 12), '1 2a 3b 4a 5 6ab 7 8a 9b 10a 11 12ab ')
