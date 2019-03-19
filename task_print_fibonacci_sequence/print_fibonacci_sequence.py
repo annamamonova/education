fe984c640c1fef68579d64c113f3e5411734d946
SEAnMa <Anna.Mamonova@ascom.com>
2019-03-19 15:40:41 +0300

    rewrite task with iterator

--- task_print_fibonacci_sequence/print_fibonacci_sequence.py
+++ task_print_fibonacci_sequence/print_fibonacci_sequence.py
@@ -1,29 +1,63 @@
 """Print Fibonacci sequence."""
 
 
-def generate_fibonacci_sequence(number=21):
+# def generate_fibonacci_sequence(max_count=21):
+#     """Function for generate Fibonacci sequence.
+#
+#     param:
+#     max_count: max number in sequence
+#     """
+#     first_number, second_number = 0, 1
+#     sequence = ''
+#     while second_number < max_count:
+#         result = first_number + second_number
+#         sequence += str(result) + ' '
+#         first_number = second_number
+#         second_number = result
+#     return sequence
+
+
+class Fibonacci:
+
+    def __init__(self, max_count=21):
+        self.first_number = 0
+        self.second_number = 1
+        self.max_count = max_count
+        self.count = 0
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        if self.count <= self.max_count:
+            result = self.first_number + self.second_number
+            self.first_number = self.second_number
+            self.second_number = result
+            self.count += 1
+            return result
+        else:
+            raise StopIteration
+
+
+def generate_fibonacci_sequence(max_count):
     """Function for generate Fibonacci sequence.
 
     param:
-    number: max number in sequence
+    max_count: max number in sequence
     """
-    first_number, second_number = 0, 1
-    sequence = ''
-    while second_number < number:
-        result = first_number + second_number
-        sequence += str(result) + ' '
-        first_number = second_number
-        second_number = result
+    sequence = []
+    for element in Fibonacci(max_count):
+        sequence.append(element)
     return sequence
 
 
-def print_fibonacci_sequence(number=21):
+def print_fibonacci_sequence(max_count=21):
     """Function for print fibonacci sequence.
 
     param:
     number: max number in sequence
     """
-    print(generate_fibonacci_sequence(number))
+    print(generate_fibonacci_sequence(max_count))
 
 
 if __name__ == '__main__':
--- task_print_fibonacci_sequence/unit_test.py
+++ task_print_fibonacci_sequence/unit_test.py
@@ -9,8 +9,8 @@ class TestGenerateFibonacciSequence(unittest.TestCase):
     """Class for unittests"""
 
     def test_sequence(self):
-        """Unittest for check Fibonacci sequence for max number 34."""
-        self.assertEqual(generate_fibonacci_sequence(34), '1 2 3 5 8 13 21 34 ')
+        """Unittest for check Fibonacci sequence for max counter 21."""
+        self.assertEqual(generate_fibonacci_sequence(21), [1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 610, 987, 1597, 2584, 4181, 6765, 10946, 17711, 28657])
 
 
 if __name__ == '__main__':
