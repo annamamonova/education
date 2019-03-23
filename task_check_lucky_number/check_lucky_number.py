4fec0db96eecd54e413951502ef7aa6d8f93193d
SEAnMa <Anna.Mamonova@ascom.com>
2019-03-23 22:06:24 +0300

    little rewrite functions

--- task_check_lucky_number/check_lucky_number.py
+++ task_check_lucky_number/check_lucky_number.py
@@ -1,16 +1,17 @@
 """Task for check lucky number."""
 
 
-def conversion(number):
+def conversion(number, dec):
     """Function for conversion number in sequence numerals
 
     :param
-    number: number for conversion.
+    number: number for conversion
+    dec: system numeral(e.g. 10, 2, 3).
     """
     conv_number = []
     while number > 0:
-        conv_number.append(number % 10)
-        number = number // 10
+        conv_number.append(number % dec)
+        number = number // dec
     return conv_number
 
 
@@ -20,9 +21,8 @@ def is_lucky_number(number):
     :param
     number: number for check.
     """
-    conv_number, sum1 = conversion(number), 0
-    for i in range(len(conv_number) // 2):
-        sum1 += conv_number[i]
+    conv_number, sum1 = conversion(number, 10), 0
+    sum1 = sum(conv_number[:(len(conv_number) // 2)])
     if sum(conv_number) - sum1 == sum1:
         return True
     return False
