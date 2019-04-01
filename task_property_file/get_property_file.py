cd134785ad268a20351b735bea703543be1553d3
SEAnMa <Anna.Mamonova@ascom.com>
2019-04-01 22:06:52 +0300

    minor change

--- task_property_file/get_property_file.py
+++ task_property_file/get_property_file.py
@@ -33,9 +33,8 @@ def get_property_file(path_file):
     except FileNotFoundError:
         print("File doesn't find.")
         raise
-    except Exception():
+    except Exception:
         print("Unknown error!")
-        raise
     return {'num_line': get_num_line(context),
             'num_word': get_num_word(context),
             'size_file': path.getsize(path_file)}
