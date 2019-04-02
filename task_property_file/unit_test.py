127ecc8afa8fcc4e6ea683f4a573335bcab35c02
SEAnMa <Anna.Mamonova@ascom.com>
2019-04-02 09:18:17 +0300

    change path to file and added .txt

--- task_property_file/unit_test.py
+++ task_property_file/unit_test.py
@@ -10,14 +10,13 @@ class TestGetPropertyFile(unittest.TestCase):
 
     def test_correct_file(self):
         """Test for correct file."""
-        self.assertEqual(get_property_file(
-            'C:\\Users\\SEAnMa\\Documents\\education\\task_property_file.txt'),
+        self.assertEqual(get_property_file('task_property_file.txt'),
                          {'num_line': 21, 'num_word': 76, 'size_file': 444})
 
     def test_not_exist(self):
         """Test for file doesn't exist."""
         with self.assertRaises(FileNotFoundError):
-            get_property_file('C:\\Users\\SEAnMa\\Documents\\education\\file.txt')
+            get_property_file('file.txt')
 
 
 if __name__ == '__main__':
