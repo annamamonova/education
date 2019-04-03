aa93536e336b3ff9ca2300e3754521b85017d2f0
SEAnMa <Anna.Mamonova@ascom.com>
2019-04-03 13:08:25 +0300

    added check OS

--- task_property_file/unit_test.py
+++ task_property_file/unit_test.py
@@ -1,6 +1,7 @@
 """Unittests for get property file."""
 
 from os import path
+import platform
 
 import unittest
 from task_property_file.get_property_file import get_property_file
@@ -17,9 +18,15 @@ class TestGetPropertyFile(unittest.TestCase):
 
     def test_correct_file_cp1251(self):
         """Test for correct file."""
-        self.assertEqual(get_property_file(path.join('task_property_file',
-                                                     'task_property_file_cp1251.txt')),
-                         {'num_line': 21, 'num_word': 76, 'size_file': 444})
+        if platform.system() == 'Windows':
+            self.assertEqual(get_property_file(path.join('task_property_file',
+                                                         'task_property_file_cp1251.txt')),
+                            {'num_line': 21, 'num_word': 76, 'size_file': 444})
+        elif platform.system() == 'Linux':
+            self.assertEqual(get_property_file(path.join('task_property_file',
+                                                         'task_property_file_cp1251.txt')),
+                             {'num_line': 21, 'num_word': 76, 'size_file': 424})
+
 
     def test_not_exist(self):
         """Test for file doesn't exist."""
